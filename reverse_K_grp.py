3eca8e268b4591e6e87abfc9b67fa0f10be16163
sujal-m <sujalmeshram612@gmail.com>
2026-07-21 15:08:31 +0530

    Reverse K Grp, LC 25

---
+++ reverse_K_grp.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head, k):
+        count = 0
+        curr = head
+        l = head
+        while count != k:
+            count += 1
+            if count == k:
+                break
+            curr = curr.next
+        firstNode = l
+        firstNode.next = curr
+        while l != curr:
+            nxt = l.next
+            prev = l
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # calculate length of LL
+        curr = head
+        length = 0
+        while curr:
+            length += 1
+            curr = curr.next
+
+        #
